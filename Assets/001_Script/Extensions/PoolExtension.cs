59515e52c10aca88a90a1cb3660db5bac36b9253
Tan Minh <m.tan2007@gmail.com>
2017-01-20 09:25:56 +0700

    reaarrange folders

--- Assets/001_Script/Extensions/PoolExtension.cs
+++ Assets/001_Script/Extensions/PoolExtension.cs
@@ -17,19 +17,15 @@ public static class PoolExtension {
 
 	public static void NextPhase(this Pool pool){
 		if (pool.isPhase01_PlayerTurn) {
-			Debug.Log ("to phase 2");
 			pool.isPhase01_PlayerTurn = false;
 			pool.isPhase02_OpponentTurn = true;
 		} else if (pool.isPhase02_OpponentTurn) {
-			Debug.Log ("to phase 3");
 			pool.isPhase02_OpponentTurn = false;
 			pool.isPhase03_MovingMovers = true;
 		} else if (pool.isPhase03_MovingMovers){
-			Debug.Log ("to phase 4");
 			pool.isPhase03_MovingMovers = false;
 			pool.isPhase04_CheckGameOver = true;
 		}else if (pool.isPhase04_CheckGameOver) {
-			Debug.Log ("to phase 1");
 			pool.isPhase04_CheckGameOver = false;
 			pool.isPhase01_PlayerTurn = true;
 		}
